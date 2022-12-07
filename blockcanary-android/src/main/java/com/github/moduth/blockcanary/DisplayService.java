eef452538b6c807e040f1f3995be0270b7c94426
xuexiangjys <xuexiangjys@163.com>
2022-12-08 01:32:41 +0800

    修复通知无法提示的问题

--- blockcanary-android/src/main/java/com/github/moduth/blockcanary/DisplayService.java
+++ blockcanary-android/src/main/java/com/github/moduth/blockcanary/DisplayService.java
@@ -17,11 +17,7 @@ package com.github.moduth.blockcanary;
 
 import static android.app.PendingIntent.FLAG_IMMUTABLE;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
-import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.HONEYCOMB;
-import static android.os.Build.VERSION_CODES.JELLY_BEAN;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -29,13 +25,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.github.moduth.blockcanary.internal.BlockInfo;
 import com.github.moduth.blockcanary.ui.DisplayActivity;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 final class DisplayService implements BlockInterceptor {
 
@@ -43,7 +38,7 @@ final class DisplayService implements BlockInterceptor {
 
     private static final String CHANNEL_ID = "block_canary_channel_id";
     private static final CharSequence CHANNEL_NAME = "block_canary_channel_name";
-
+    private static final int NOTIFY_ID = 100001;
     @Override
     public void onBlock(Context context, BlockInfo blockInfo) {
         Intent intent = new Intent(context, DisplayActivity.class);
@@ -55,46 +50,20 @@ final class DisplayService implements BlockInterceptor {
         show(context, contentTitle, contentText, pendingIntent);
     }
 
-    @TargetApi(HONEYCOMB)
     private void show(Context context, String contentTitle, String contentText, PendingIntent pendingIntent) {
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Notification notification;
-        if (SDK_INT < HONEYCOMB) {
-            notification = new Notification();
-            notification.icon = R.drawable.block_canary_notification;
-            notification.when = System.currentTimeMillis();
-            notification.flags |= Notification.FLAG_AUTO_CANCEL;
-            notification.defaults = Notification.DEFAULT_SOUND;
-            try {
-                Method deprecatedMethod = notification.getClass().getMethod("setLatestEventInfo", Context.class, CharSequence.class, CharSequence.class, PendingIntent.class);
-                deprecatedMethod.invoke(notification, context, contentTitle, contentText, pendingIntent);
-            } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException
-                    | InvocationTargetException e) {
-                Log.w(TAG, "Method not found", e);
-            }
-        } else {
-            Notification.Builder builder = new Notification.Builder(context)
-                    .setSmallIcon(R.drawable.block_canary_notification)
-                    .setWhen(System.currentTimeMillis())
-                    .setContentTitle(contentTitle)
-                    .setContentText(contentText)
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent)
-                    .setDefaults(Notification.DEFAULT_SOUND);
-            if (SDK_INT < JELLY_BEAN) {
-                notification = builder.getNotification();
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-                channel.enableVibration(false);
-                channel.enableLights(false);
-                notificationManager.createNotificationChannel(channel);
-                notification = builder.build();
-            } else {
-                notification = builder.build();
-            }
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.block_canary_notification)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_SOUND).build();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            managerCompat.createNotificationChannel(channel);
         }
-        notificationManager.notify(0xDEAFBEEF, notification);
+        managerCompat.notify(NOTIFY_ID, notification);
     }
 }
